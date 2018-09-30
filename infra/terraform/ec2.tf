15d04330ef1fd39dd82afa0f622d40fd97f4ec3c
sizer <shinse.tanaka@gmail.com>
2018-09-30 19:04:54 +0900

    install capistrano-rails
    bundle exec cap install

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -54,6 +54,13 @@ resource "aws_security_group" "public_ssh_connection" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "full_open_outbound" {
